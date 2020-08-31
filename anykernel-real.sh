d118e25a3e45a743e1f3bd39ae9b56664c5a16a8
ZyCromerZ <neetroid97@gmail.com>
2020-08-31 20:16:02 +0700

    prepare for X00TD devices
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -10,9 +10,9 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=X01BD
-device.name2=X01BDA
-device.name3=
+device.name1=X00T
+device.name2=X00TD
+device.name3=ASUS_X00TD
 device.name4=
 device.name5=
 supported.versions=
@@ -20,7 +20,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/platform/soc/c0c4000.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -29,11 +29,16 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Mount partitions as rw
+mount /system;
+mount /vendor;
+mount -o remount,rw /system;
+mount -o remount,rw /vendor;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 755 755 $ramdisk/init* $ramdisk/sbin;
 
 
 ## AnyKernel install
@@ -41,58 +46,58 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
+#Remove old kernel stuffs from ramdisk
+ui_print "cleaning up..."
+ rm -rf $ramdisk/init.special_power.sh
+ rm -rf $ramdisk/init.darkonah.rc
+ rm -rf $ramdisk/init.spectrum.rc
+ rm -rf $ramdisk/init.spectrum.sh
+ rm -rf $ramdisk/init.boost.rc
+ rm -rf $ramdisk/init.trb.rc
+ rm -rf $ramdisk/init.azure.rc
+ rm -rf $ramdisk/init.PBH.rc
+ rm -rf $ramdisk/init.Pbh.rc
+ rm -rf $ramdisk/init.overdose.rc
+
 backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+remove_line init.rc "import /init.darkonah.rc";
+remove_line init.rc "import /init.spectrum.rc";
+remove_line init.rc "import /init.boost.rc";
+remove_line init.rc "import /init.trb.rc"
+remove_line init.rc "import /init.azure.rc"
+remove_line init.rc "import /init.PbH.rc"
+remove_line init.rc "import /init.Pbh.rc"
+remove_line init.rc "import /init.overdose.rc"
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+ui_print " ";
+# rearm perfboostsconfig.xml
+if [ ! -f /vendor/etc/perf/perfboostsconfig.xml ]; then
+	mv /vendor/etc/perf/perfboostsconfig.xml.bak /vendor/etc/perf/perfboostsconfig.xml;
+	mv /vendor/etc/perf/perfboostsconfig.xml.bkp /vendor/etc/perf/perfboostsconfig.xml;
+fi
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# rearm commonresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/commonresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bak /vendor/etc/perf/commonresourceconfigs.xml;
+	mv /vendor/etc/perf/commonresourceconfigs.xml.bkp /vendor/etc/perf/commonresourceconfigs.xml;
+fi
 
-# remove spectrum profile
-if [ -e $ramdisk/init.spectrum.rc ];then
-  rm -rf $ramdisk/init.spectrum.rc
-  ui_print "delete /init.spectrum.rc"
+# rearm targetconfig.xml
+if [ ! -f /vendor/etc/perf/targetconfig.xml ]; then
+	mv /vendor/etc/perf/targetconfig.xml.bak /vendor/etc/perf/targetconfig.xml;
+	mv /vendor/etc/perf/targetconfig.xml.bkp /vendor/etc/perf/targetconfig.xml;
 fi
-if [ -e $ramdisk/init.spectrum.sh ];then
-  rm -rf $ramdisk/init.spectrum.sh
-  ui_print "delete /init.spectrum.sh"
+
+# rearm targetresourceconfigs.xml
+if [ ! -f /vendor/etc/perf/targetresourceconfigs.xml ]; then
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bak /vendor/etc/perf/targetresourceconfigs.xml;
+	mv /vendor/etc/perf/targetresourceconfigs.xml.bkp /vendor/etc/perf/targetresourceconfigs.xml;
 fi
-if [ -e $ramdisk/sbin/init.spectrum.rc ];then
-  rm -rf $ramdisk/sbin/init.spectrum.rc
-  ui_print "delete /sbin/init.spectrum.rc"
-fi
-if [ -e $ramdisk/sbin/init.spectrum.sh ];then
-  rm -rf $ramdisk/sbin/init.spectrum.sh
-  ui_print "delete /sbin/init.spectrum.sh"
-fi
-if [ -e $ramdisk/etc/init.spectrum.rc ];then
-  rm -rf $ramdisk/etc/init.spectrum.rc
-  ui_print "delete /etc/init.spectrum.rc"
-fi
-if [ -e $ramdisk/etc/init.spectrum.sh ];then
-  rm -rf $ramdisk/etc/init.spectrum.sh
-  ui_print "delete /etc/init.spectrum.sh"
-fi
-if [ -e $ramdisk/init.aurora.rc ];then
-  rm -rf $ramdisk/init.aurora.rc
-  ui_print "delete /init.aurora.rc"
-fi
-if [ -e $ramdisk/sbin/init.aurora.rc ];then
-  rm -rf $ramdisk/sbin/init.aurora.rc
-  ui_print "delete /sbin/init.aurora.rc"
-fi
-if [ -e $ramdisk/etc/init.aurora.rc ];then
-  rm -rf $ramdisk/etc/init.aurora.rc
-  ui_print "delete /etc/init.aurora.rc"
+
+# rearm powerhint.xml
+if [ ! -f /vendor/etc/powerhint.xml ]; then
+	mv /vendor/etc/powerhint.xml.bak /vendor/etc/powerhint.xml;
+	mv /vendor/etc/powerhint.xml.bkp /vendor/etc/powerhint.xml;
 fi
 
 # end ramdisk changes
