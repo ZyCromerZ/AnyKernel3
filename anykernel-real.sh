b5efaeb4186ee25b11efad6c088654f6d699e1b6
ZyCromerZ <neetroid97@gmail.com>
2021-09-26 14:17:56 +0700

    update - ignore gpu clock changes for Neutrino-Stock - patch androidboot.selinux to enforcing if zip has Enforcing word or SecurityCoreAdd.apk file available
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -48,36 +48,47 @@ do
     X=$(($X-1));
 done
 
-# 0 900Mhz
-# 1 897Mhz
-# 2 892Mhz
-# 3 888Mhz
-# 4 884Mhz
-# 5 880Mhz
-# 6 875Mhz
-# 7 871Mhz
-# 8 867Mhz
-# 9 862Mhz
-# 10 858Mhz
-# 11 854Mhz
-# 12 850Mhz
-# 13 835Mhz
-# 14 821Mhz
-# 15 806Mhz (default)
-ui_print " ";
-GpuFreq="15";
-no=0;
-for ListFreq in 900 897 892 888 884 880 875 871 867 862 858 854 850 835 821 806; do
-    if [ ! -z "$(cat /tmp/zyc_kernelname | grep "${ListFreq}Mhz" )" ];then
-        GpuFreq="$no";
-        [ "$no" != "15" ] && ui_print "GPU: set max clock to ${ListFreq}Mhz(oc)";
-        [ "$no" == "15" ] && ui_print "use standard max clock 806Mhz(stock)";
-        break;
-    fi
-    no=$(($no+1));
-done
+if [ -z "$(cat /tmp/zyc_kernelname | grep "Neutrino-Stock" )" ];then
+    # 0 900Mhz
+    # 1 897Mhz
+    # 2 892Mhz
+    # 3 888Mhz
+    # 4 884Mhz
+    # 5 880Mhz
+    # 6 875Mhz
+    # 7 871Mhz
+    # 8 867Mhz
+    # 9 862Mhz
+    # 10 858Mhz
+    # 11 854Mhz
+    # 12 850Mhz
+    # 13 835Mhz
+    # 14 821Mhz
+    # 15 806Mhz (default)
+    ui_print " ";
+    GpuFreq="15";
+    no=0;
+    for ListFreq in 900 897 892 888 884 880 875 871 867 862 858 854 850 835 821 806; do
+        if [ ! -z "$(cat /tmp/zyc_kernelname | grep "${ListFreq}Mhz" )" ];then
+            GpuFreq="$no";
+            [ "$no" != "15" ] && ui_print "GPU: set max clock to ${ListFreq}Mhz(oc)";
+            [ "$no" == "15" ] && ui_print "use standard max clock 806Mhz(stock)";
+            break;
+        fi
+        no=$(($no+1));
+    done
+    patch_cmdline "zyc.gpu_clock" "zyc.gpu_clock=$GpuFreq";
+fi
 
-patch_cmdline "zyc.gpu_clock" "zyc.gpu_clock=$GpuFreq";
+if [ ! -z "$(cat /tmp/zyc_kernelname | grep "Enforcing" )" ] || [ -f /system_root/system/app/SecurityCoreAdd/SecurityCoreAdd.apk ] || [ -f /system/app/SecurityCoreAdd/SecurityCoreAdd.apk ];then
+    X=10;
+    while [ $X != 0 ];
+    do
+        patch_cmdline "androidboot.selinux" " ";
+        X=$(($X-1));
+    done
+    patch_cmdline "androidboot.selinux" "androidboot.selinux=enforcing";
+fi
 
 rm -rf /tmp/zyc_kernelname;
 
