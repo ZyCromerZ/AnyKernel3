120a3d3ae890600efc85277fd1eac1ff2d213c6d
ZyCromerZ <neetroid97@gmail.com>
2021-11-19 15:45:27 +0700

    Add max gpu changer
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -41,6 +41,43 @@ dump_boot;
 
 # begin ramdisk changes
 
+X=10;
+while [ $X != 0 ];
+do
+    patch_cmdline "zyc.gpu_clock" " ";
+    X=$(($X-1));
+done
+
+if [ ! -z "$(cat /tmp/zyc_kernelname | grep "Neutrino" )" ];then
+    # 1100
+    # 1075
+    # 1050
+    # 1025
+    # 1000 (default)
+    # 975
+    # 950
+    # 925
+    # 900
+    # 875
+    # 850
+    # 823
+    # 796
+    ui_print " ";
+    GpuFreq="-1";
+    no=0;
+    for ListFreq in 1100 1075 1050 1025 1000 975 950 925 900 875 850 823 796; do
+        if [ ! -z "$(cat /tmp/zyc_kernelname | grep "${ListFreq}Mhz" )" ];then
+            GpuFreq="$no";
+            [ "$ListFreq" > "950" ] && ui_print "GPU: set max clock to ${ListFreq}Mhz(oc)";
+            [ "$ListFreq" <= "950" ] && ui_print "GPU: set max clock to ${ListFreq}Mhz";
+            break;
+        fi
+        no=$(($no+1));
+    done
+    patch_cmdline "zyc.gpu_clock" "zyc.gpu_clock=$GpuFreq";
+fi
+
+rm -rf /tmp/zyc_kernelname;
 # end ramdisk changes
 
 write_boot;
