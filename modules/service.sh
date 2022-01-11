72bfd7e7c3546733ef4a9a43db26abefd64841bc
ZyCromerZ <neetroid97@gmail.com>
2022-01-11 11:32:56 +0700

    Revert "add service to write to something"
    This reverts commit ed4174e512bf6f35a7dc174bec46d230b41f3c6d.
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- modules/service.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-WriteTo()
-{
-    echo "$2" > "$1"
-}
-
-# Report max frequency to unity tasks.
-WriteTo /proc/sys/kernel/sched_lib_name "com.miHoYo,UnityMain,libunity.so"
-WriteTo /proc/sys/kernel/sched_lib_mask_force 255
