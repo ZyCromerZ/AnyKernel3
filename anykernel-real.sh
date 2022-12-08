4d546d12a0c879b804b2bb351e508066bd1d22bb
ZyCromerZ <neetroid97@gmail.com>
2022-12-08 08:38:20 +0700

    fix block path after update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -28,7 +28,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/platform/bootdevice/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
